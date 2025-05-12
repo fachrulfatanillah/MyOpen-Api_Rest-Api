37f31bd7207669644b2d3d5dc2506c078775cfae
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-13 02:16:17 +0700

    Create migrations project $ model project

---
+++ app/Models/Project.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Str;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $table = 'projects';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'unicode',
+        'project_name',
+        'status',
+        'created',
+        'updated',
+        'user_id',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($project) {
+            $project->unicode = (string) Str::uuid();
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(Users::class, 'user_id', 'unicode');
+    }
+}
---
+++ database/migrations/2025_05_13_004832_create_projects_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('unicode')->unique();
+            $table->string('project_name', 100);
+            $table->tinyInteger('status');
+            $table->timestamp('created')->useCurrent();
+            $table->timestamp('updated')->useCurrent()->useCurrentOnUpdate();
+
+            $table->uuid('user_id');
+            $table->foreign('user_id')->references('unicode')->on('users')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
