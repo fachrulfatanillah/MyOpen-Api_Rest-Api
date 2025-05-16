e90551ac5c46314543f4ac79bff190686b4fbc2a
fachrul fatanillah <87415039+fachrulfatanillah@users.noreply.github.com>
2025-05-17 02:53:13 +0700

    create migration project_name_log & project_is_active_log

--- database/migrations/2025_05_13_004832_create_projects_table.php
+++ database/migrations/2025_05_13_004832_create_projects_table.php
@@ -18,10 +18,30 @@ return new class extends Migration {
             $table->uuid('user_id');
             $table->foreign('user_id')->references('unicode')->on('users')->onDelete('cascade')->onUpdate('cascade');
         });
+
+        Schema::create('project_name_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('project_name', 100);
+            $table->timestamp('created')->useCurrent();
+            $table->uuid('project_id');
+
+            $table->foreign('project_id')->references('unicode')->on('projects')->onDelete('cascade')->onUpdate('cascade');
+        });
+
+        Schema::create('project_is_active_log', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->tinyInteger('is_active');
+            $table->timestamp('created')->useCurrent();
+            $table->uuid('project_id');
+
+            $table->foreign('project_id')->references('unicode')->on('projects')->onDelete('cascade')->onUpdate('cascade');
+        });
     }
 
     public function down(): void
     {
         Schema::dropIfExists('projects');
+        Schema::dropIfExists('project_name_log');
+        Schema::dropIfExists('project_is_active_log');
     }
 };
